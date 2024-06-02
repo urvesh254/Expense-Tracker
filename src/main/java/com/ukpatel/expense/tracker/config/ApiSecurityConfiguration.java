5d0eb3026705ca7b259a05cb69d0fb56e5e79a15
urvesh254 <urvesh254@gmail.com>
2024-06-02 11:41:03 +0530

    Initial server up configuration completed including ApiSecurityConfiguration.

---
+++ src/main/java/com/ukpatel/expense/tracker/config/ApiSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.ukpatel.expense.tracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ApiSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+                .sessionManagement(sessionConfigure -> sessionConfigure.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
